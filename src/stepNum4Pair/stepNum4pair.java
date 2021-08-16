6566a95a8389edd6e768e5eace39e089b55cfad8
张楗豪29857 <offergiles@hotmail.com>
2021-08-15 22:39:37 -0700

    添加字符匹配备忘录

--- src/stepNum4Pair/stepNum4pair.java
+++ src/stepNum4Pair/stepNum4pair.java
@@ -2,6 +2,8 @@ package stepNum4Pair;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+
 /**
  *  可以用增删改进行修改s1字符串使她和s2字符串一样  计算最小的需要的步数
  *  base: 当任意一个走完 只需要删除或者增加剩余一个字符串的个数即可确认最后增删改操作步数return
@@ -10,19 +12,24 @@ import org.junit.Test;
  */
 public class stepNum4pair {
 
-    public int dp(int[] s1, int[] s2, int index1, int index2){
+    public int dp(int[] s1, int[] s2, int index1, int index2, int[][] mem){
         if(index1 == -1) return index2+1;
         if(index2 == -1) return index1+1;
         //取增删改一次以后各自接下来最小的改动步数
-        if(s1[index1] == s2[index2]) return dp(s1,s2, index1-1,index2-1);
+        if(s1[index1] == s2[index2]) return dp(s1,s2, index1-1,index2-1,mem);
         else {
-            //如果是增加
-            int increase = dp(s1, s2, index1, index2 - 1) + 1;
-            //如果是删除
-            int delete = dp(s1, s2, index1 - 1, index2) + 1;
-            //如果是更改
-            int update = dp(s1, s2, index1 - 1, index2 - 1) + 1;
-            return Math.min(Math.min(increase, delete), update);
+            if(mem[index1][index2] != -666){
+                return mem[index1][index2];
+            }else {
+                //如果是增加
+                int increase = dp(s1, s2, index1, index2 - 1,mem) + 1;
+                //如果是删除
+                int delete = dp(s1, s2, index1 - 1, index2,mem) + 1;
+                //如果是更改
+                int update = dp(s1, s2, index1 - 1, index2 - 1,mem) + 1;
+                mem[index1][index2] = Math.min(increase,Math.min(delete,update));
+                return mem[index1][index2];
+            }
         }
     }
 
@@ -33,7 +40,13 @@ public class stepNum4pair {
         int index = chars.length-1;
         int[] arr1= {1,3,3};
         int[] arr2= {3,2,3};
-        int dp = dp(arr1,arr2,2,2);
+        int[][] mem = new int[100][100];
+        for (int[] ints : mem) {
+            for (int anInt : ints) {
+                anInt = -666;
+            }
+        }
+        int dp = dp(arr1,arr2,2,2,mem);
         System.out.println(dp);
     }
 }
