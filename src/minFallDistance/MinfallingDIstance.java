a91f1f62d34d74b8a2ba83381f9e17ac077413ab
张楗豪29857 <offergiles@hotmail.com>
2021-08-16 12:15:12 -0700

    达到最小距离

--- src/minFallDistance/MinfallingDIstance.java
+++ src/minFallDistance/MinfallingDIstance.java
@@ -8,33 +8,37 @@ import org.junit.Test;
  * @author ericzhang 2021/08/16 9:16 上午
  */
 public class MinfallingDIstance {
-
-        public int dp(int[][] matrix, int row, int column){
-        if(row == 0) return matrix[0][column];
+    int mem[][] = new int[1000][1000];
+    public int dp(int[][] matrix, int row, int column) {
+        if (row == 0) return matrix[0][column];
+        if(mem[row][column] != 0){
+            return mem[row][column];
+        }
         int leftUp = 66666;
         int rightUp = 66666;
         int straight = 66666;
-        if(column > 0 && column <= 2 && row > 0 && row <= 2) {
+        if (column > 0 && column <= 2 && row > 0 && row <= 2) {
             leftUp = dp(matrix, row - 1, column - 1) + matrix[row][column];
         }
-        if(column >= 0 && column <= 2 && row > 0 && row <= 2) {
+        if (column >= 0 && column <= 2 && row > 0 && row <= 2) {
             straight = dp(matrix, row - 1, column) + matrix[row][column];
         }
-        if(column >= 0 && column < 2 && row > 0 && row <= 2) {
+        if (column >= 0 && column < 2 && row > 0 && row <= 2) {
             rightUp = dp(matrix, row - 1, column + 1) + matrix[row][column];
         }
-        return Math.min(leftUp,Math.min(rightUp,straight));
+        mem[row][column] =  Math.min(leftUp, Math.min(rightUp, straight));
+        return mem[row][column];
     }
 
     @Test
     public void test() {
-        int[][] matrix ={{2,1,3},{6,5,4},{7,8,9}};
+        int[][] matrix = {{2, 1, 3}, {6, 5, 4}, {7, 8, 9}};
         int row = matrix.length - 1;
         int column = matrix[0].length - 1;
         //从最下层往上走 起点可能是最下层的任意一点
         int j = 0;
         int res = 99999;
-        for(j = 0;j < 3; j++){
+        for (j = 0; j < 3; j++) {
             res = Math.min(res, dp(matrix, row, j));
         }
         System.out.println(res);
