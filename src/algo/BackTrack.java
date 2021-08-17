b4d645b77ead02fae38b4176067232363fdafe06
张楗豪29857 <offergiles@hotmail.com>
2021-08-17 10:50:13 -0700

    回溯暴力穷举算法

---
+++ src/algo/BackTrack.java
@@ -0,0 +1,21 @@
+package algo;
+
+/**
+ * 不求最值 求全排列  类似DFS算法  回溯就是全遍历  选了1之后有2,3两个选项，那就进入递归把2,3进行全组合枚举， 通过限制条件限制，选到叶子节点后进行叶子节点删除  方便下一种情况
+ * @author ericzhang 2021/08/16 6:14 下午
+ */
+
+// void backTrack(路径，选择列表)
+//    if(满足结束条件){
+//        res.add)track);
+//        return;
+//    }
+//    for(选择 in 选择列表){
+//        做选择
+//         backTrack(路径，选择列表);
+//        撤销选择;
+//    }
+public class BackTrack {
+
+
+}
