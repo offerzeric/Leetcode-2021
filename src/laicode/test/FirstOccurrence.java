5bfe4b9f4262f4d3563ce050210014b74676b3bc
张楗豪29857 <offergiles@hotmail.com>
2022-01-18 22:10:34 -0800

    2022.1.18

--- src/laicode/test/FirstOccurrence.java
+++ src/laicode/test/FirstOccurrence.java
@@ -1,5 +1,7 @@
 package laicode.test;
 
+import java.util.Arrays;
+
 /**
  * description
  *
@@ -14,6 +16,8 @@ public class FirstOccurrence {
       }
       int start = 0;
       int end = array.length - 1;
+      //start <= end 是做binary search时进行单个元素查找的条件
+      //start < end-1是查范围的时候用的查找条件
       while (start < end - 1) {
          int mid = start + (end - start) / 2;
          if (array[mid] < target) {
