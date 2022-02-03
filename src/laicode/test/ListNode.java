317413e5fd328c3f55e1ad9c46ee66a7f6e4a214
张楗豪29857 <offergiles@hotmail.com>
2022-02-02 23:32:38 -0800

    2022.2.2

--- src/laicode/test/ListNode.java
+++ src/laicode/test/ListNode.java
@@ -5,11 +5,12 @@ package laicode.test;
  *
  * @author ericzhang 2022/01/27 14:43
  */
-    public  class ListNode {
-       public int value;
-       public ListNode next;
-       public ListNode(int value) {
-         this.value = value;
-         next = null;
-       }
-     }
+public class ListNode {
+   public int value;
+   public ListNode next;
+
+   public ListNode(int value) {
+      this.value = value;
+      next = null;
+   }
+}
